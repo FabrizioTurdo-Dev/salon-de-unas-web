719b24cafcab6bcc3f54dbe54365ffae3456c342
Your Name <you@example.com>
2025-12-20 15:24:23 -0300

    landing completa con secciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Hero from './components/Hero.jsx';
 import ServicesSelector from './components/ServicesSelector.jsx';
+import Location from './components/Location';
+import Footer from './components/Footer';
+
 function App (){
   return(
     <>
       <Hero/>
       <ServicesSelector/>
+      <Location/>
+      <Footer/>
     </>
   )
 }
