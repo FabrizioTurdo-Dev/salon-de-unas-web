9a3894d771e0c582b151de214af47d0cadc7ec1f
Your Name <you@example.com>
2025-12-20 15:29:38 -0300

    modificamos app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Hero from './components/Hero.jsx';
 import ServicesSelector from './components/ServicesSelector.jsx';
+import Location from './components/Location.jsx';
+import Footer from './components/Footer.jsx';
 function App (){
   return(
     <>
       <Hero/>
       <ServicesSelector/>
+      <Location/>
+      <Footer/>
     </>
   )
 }
